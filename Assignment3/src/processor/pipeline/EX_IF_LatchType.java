70e8f87a7d77fea8752d0d10455d344f92a70dd8
MohithVelivela <vmohithnagasai@gmail.com>
2024-03-02 18:30:05 +0530

    Some part of Execute Unit

--- Assignment3/src/processor/pipeline/EX_IF_LatchType.java
+++ Assignment3/src/processor/pipeline/EX_IF_LatchType.java
@@ -1,10 +1,32 @@
 package processor.pipeline;
 
 public class EX_IF_LatchType {
+
+	boolean IF_Enable;
+
+	int Branchtarget;
+	boolean IsBranchTaken;
 	
 	public EX_IF_LatchType()
 	{
-		
+		IF_Enable = false;
+	}
+	public void setBranchtarget(int branchtarget){
+		Branchtarget = branchtarget;
+	}
+	public int getBranchtarget(){
+		return Branchtarget;
 	}
 
-}
+	public void setBranchTaken(boolean branchTaken){
+		IsBranchTaken = branchTaken;
+	}
+
+	public boolean getBranchTaken()
+	{
+		return IsBranchTaken;
+	}
+
+	}
+
+
